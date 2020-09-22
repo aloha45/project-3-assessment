5c6387a8dc6ab28fe775a026959b45b7e7a83319
Cory Spicer <corys4139@gmail.com>
2020-09-22 14:23:25 -0400

    Updates

---
+++ precache-manifest.4ae6c9a369ece08e2ff21cbbadedfcb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e42a9009aa4be6f0847376ab73a9a02",
+    "url": "/project-3-assessment/index.html"
+  },
+  {
+    "revision": "048d91432e1118b4cec7",
+    "url": "/project-3-assessment/static/css/main.69e725f6.chunk.css"
+  },
+  {
+    "revision": "eedcdc39bfaa1c119000",
+    "url": "/project-3-assessment/static/js/2.379313e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-3-assessment/static/js/2.379313e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048d91432e1118b4cec7",
+    "url": "/project-3-assessment/static/js/main.3800afe1.chunk.js"
+  },
+  {
+    "revision": "1ade8e9aae7de799f3ee",
+    "url": "/project-3-assessment/static/js/runtime-main.471a3580.js"
+  }
+]);
